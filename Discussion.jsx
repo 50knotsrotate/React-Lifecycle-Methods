ebb01bb766f38570b5c915b599d45dce0c490efc
50knotsrotate <42186149+50knotsrotate@users.noreply.github.com>
2019-01-06 14:56:05 -0500

    Using componentDidMount & componentWillUnmount
    Using lifecycle methods to start & stop a live feed on a component to prevent memory leaks

---
+++ Discussion.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Discussion extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+            pageTitle: 'Discussion',
+            currentTime: String(new Date())
+        }
+
+        
+    }
+    componentDidMount(){
+        
+       this.liveFeed =  setInterval(() => {
+            console.log('new chat message')
+            this.setState({
+                currentTime:String(new Date())
+            })
+        }, 1000)
+    }
+
+   componentWillUnmount(){
+       clearInterval(this.liveFeed)
+   }
+    render(){
+        const {pageTitle, currentTime} = this.state;
+        return(
+            <div>
+               <h1>{pageTitle}</h1>
+               <div>
+                   {currentTime}
+               </div>
+            </div>
+        )
+    }
+}
+
+export default Discussion;
